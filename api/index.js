0995fce9c5d54f3dcfd32726967d84ed2fc329ce
Victor Falconi <v.falconi.m@gmail.com>
2023-01-16 09:49:59 -0500

    cors

--- api/index.js
+++ api/index.js
@@ -8,6 +8,14 @@ const morgan = require('morgan')
 
 server.use(morgan("dev"));
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
 // port va a buscar un puerto que nos de el servicio de hosting donde deployamos, si no, toma el que le pasemos
 // const PORT = process.env.PORT || 9000;
 
