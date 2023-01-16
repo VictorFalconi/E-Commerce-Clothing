0164e7b6b4acbf4553a1bf56d625a1f2b55e197e
Victor Falconi <v.falconi.m@gmail.com>
2023-01-16 09:14:14 -0500

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -12,8 +12,15 @@ const cors = require('cors')
 
 const server = express();
 
-server.use(cors())
+//server.use(cors())
 server.use(morgan("dev"));
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
 
 //ruta de prueba
 server.get("/people", (req, res) => {
