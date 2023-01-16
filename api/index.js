6e75e4cdb9bf42222cde18fef01c4f6e6d56b84e
Victor Falconi <v.falconi.m@gmail.com>
2023-01-16 09:52:14 -0500

    cors

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('cors');
 
 const server = require('./src/app')
 
@@ -8,13 +9,7 @@ const morgan = require('morgan')
 
 server.use(morgan("dev"));
 
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+server.use(cors({origin: '*'}));
 
 // port va a buscar un puerto que nos de el servicio de hosting donde deployamos, si no, toma el que le pasemos
 // const PORT = process.env.PORT || 9000;
