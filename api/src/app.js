ede8ab34e880eb21934c5bccae9410147f49bd2c
Victor Falconi <v.falconi.m@gmail.com>
2023-01-17 12:34:30 -0500

    cambios

--- api/src/app.js
+++ api/src/app.js
@@ -7,22 +7,15 @@ const cartRouter = require('./routes/cart')
 const mercadoPagoRouter = require('./routes/mercadoPago')
 const buggyRouter = require('./routes/buggy')
 //const morgan = require('morgan')
-//const cors = require('cors');
+const cors = require('cors');
 
 
 const server = express();
 server.use(express.json());
 
 //server.use(cors())
-//server.use(cors({origin: '*'}));
+server.use(cors({origin: '*'}));
 //server.use(morgan("dev"));
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
 
 //ruta de prueba
 // server.get("/people", (req, res) => {
