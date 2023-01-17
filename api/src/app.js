7b9802ef3369d9864185973d85dacdb4cae0f541
Victor Falconi <v.falconi.m@gmail.com>
2023-01-17 11:18:17 -0500

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -12,6 +12,14 @@ const buggyRouter = require('./routes/buggy')
 
 const server = express();
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 //server.use(cors())
 //server.use(cors({origin: '*'}));
 //server.use(morgan("dev"));
