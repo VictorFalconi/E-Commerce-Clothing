cf38bbbccc7b24c58fda3ecd44290f1200f959d2
Victor Falconi <v.falconi.m@gmail.com>
2023-01-18 08:06:37 -0500

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -13,17 +13,17 @@ const cors = require('cors');
 const server = express();
 server.use(express.json());
 
-//server.use(cors())
+server.use(cors())
 //server.use(cors({origin: '*'}));
 server.use(morgan("dev"));
 
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+// server.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 
 //ruta de prueba
 // server.get("/people", (req, res) => {
