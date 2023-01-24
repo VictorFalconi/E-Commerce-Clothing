94c7767bac0d0e0b1f9c3e273cc48e30ceb5f5a9
MZacca <mzacca.95@gmail.com>
2023-01-24 19:23:18 -0300

    feedback front

--- client/src/components/RateUs/RateUs.jsx
+++ client/src/components/RateUs/RateUs.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { addRate, getRate } from '../../redux/actions';
 
 
 
 function RateUs() {
     const dispatch = useDispatch();
-
+    
+    useEffect(() => {
+        dispatch(getRate());
+    }, []);
+    
     return (
-        <div>Componente review compra</div>
+        <div>
+            <h1>Componente review compra</h1>
+            
+        </div>
     )
 }
+
 export default RateUs;
